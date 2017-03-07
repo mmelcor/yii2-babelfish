549b56f12f56b7ffaa7f44eb5b2d08ed256e90b7
Kev <kpayne@rainintl.com>
2017-03-07 11:01:10 -0700

    removed unneeded base folders and code

--- web/js/loginBackground.js
+++
@@ -1,9 +0,0 @@
-function backgroundSwitch() {
-    var currentTime = new Date();
-    var hours = currentTime.getHours();
-    if (hours > 6 && hours < 18) {
-	$('.login-page').addClass('day');
-    } else {
-	$('.login-page').addClass('night');
-    }
-}
