6b4d4ab92794af0a089a2ee745fb112059ef5d53
Jeremiah Payne <mmelcor@gmail.com>
2017-03-30 10:25:07 -0600

    Fixed url for update action.

--- views/translations/index.php
+++ views/translations/index.php
@@ -45,7 +45,7 @@ $dataProvider->sort->route = '../../babel/translations/index';
 		],
 		'urlCreator' => function ($action, $model, $key, $index) {
 		    if ($action === 'update') {
-			$url ='update?id='.$model->id;
+			$url ='translations/update?id='.$model->id;
 			return $url;
 		    }
 		}
